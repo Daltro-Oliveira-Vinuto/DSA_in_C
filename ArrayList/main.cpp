c4f07d7c7cc51db97199b55db2daa2b1563fc52d
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2019-06-05 18:45:20 -0300

    O(1) functions

--- ArrayList/main.cpp
+++ ArrayList/main.cpp
@@ -2,15 +2,37 @@
 
 #include <iostream>
 #include <iomanip>
-#include <cstdint>
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "staticList.hpp"
+#include "ArrayList.hpp"
 
-
-
-int main() {
+int main(int argc, char **argv) {
+  List* list = NULL;
+  Student student_aux;
+  int total = 5;
+  Student array[total] = {{111, "Galileu", 5.0, 5.0, 5.0},
+                      {222, "Newton", 6.0, 6.0, 6.0},
+                      {333, "Maxwell", 7.0, 7.0, 7.0},
+                      {444, "Faraday", 8.0, 8.0, 8.0},
+                      {555, "Newton", 9.0 , 9.0, 9.0}};
   
+  list = new_list(total);
+  if (list != NULL) {
+    print_list(list);
+    for (int i = 0; i < total; i+= 1) {
+      add_begin(list, array[i]);
+      print_list(list);
+    }
+    if (Working == search_pos(list, 5, &student_aux)) {
+      print_student(student_aux);
+    }
+    for (int i = 0; i < total; i+= 1) {
+      remove_begin(list);
+      print_list(list);
+    }
+  }
+
+  delete_list(list);
   return 0;
 }
--- ArrayList/staticList.cpp
+++ ArrayList/staticList.cpp
@@ -1,58 +1,170 @@
 
 #include <iostream>
 #include <iomanip>
-#include <cstdint>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "staticList.hpp"
+#include "ArrayList.hpp"
 
-int read_file(char* file, Student **s) {
-  FILE *fp , *fpb ;
-  char fileb[21];
-  int i = 0;
-
-  strcpy(fileb,"");
-  strcat(fileb,file);
-  strcat(fileb,".bin");
-  strcat(file,".txt");
-  fp = fopen(file, "r");
-  fpb = fopen(fileb, "w+b");
-  if (fp != NULL) {
-    // fseek(fp, 0, SEEK_SET);
-    // fseek(fpb, 0, SEEK_SET);
-    Student aux;
-    memset(aux.name,'\0' , sizeof(aux.name));
-    while(fscanf(fp, "%d %21s %lf %lf %lf", &aux.reg,
-                                      aux.name,
-                                      &aux.grade[0], &aux.grade[1], &aux.grade[2]) == 5) {
-
-      fwrite(&aux, sizeof(Student), 1, fpb);
-      //memset(aux.name, 0, sizeof(aux.name));
-    }
-    fclose(fp);
-    fp = NULL;
-    fclose(fpb);
-    fpb = NULL;
-    fpb = fopen(fileb, "r+b");
-    // fseek(fpb, 0L, SEEK_SET);
-    i = 0;
-    while (fread(&aux, sizeof(Student), 1, fpb) == 1) {
-      if (i == 0) {
-        *s = (Student*) malloc(sizeof(Student));
-      } else {
-        *s = (Student*) realloc(*s, sizeof(Student)*(i+1));
-      }
-      *(*s + i) = aux;
-      i+= 1;
-      //fseek(fpb, sizeof(Student)*i, SEEK_SET);
-    }
+// create a new list and delete a list ====================================================================
+List* new_list(int max) {
+  List *list = NULL;
+  list = (List*) malloc(sizeof(List));
+  if (list != NULL) {
+    list->max = max;
+    list->size = 0;
+    list->array = (Student*) malloc(list->max*sizeof(Student));
   }
-  fclose(fpb);
-  fpb = NULL;
-  return i;
+  return list;
 }
 
-int sum(int a, int b) {
-  return a + b;
+void delete_list(List* list) {
+  if (list != NULL) {
+    free(list->array);
+    list->array = NULL;
+    free(list);
+    list = NULL;
+  }
 }
+
+// check if a list is empty or full      =================================================================================
+Logic empty_list(List* list) {
+  Logic logic;
+  if (list->size == 0) {
+    logic = True;
+  } else {
+    logic = False;
+  }
+  return logic;
+}
+
+Logic full_list(List* list) {
+  Logic logic;
+  if (list->size == list->max) {
+    logic = True;
+  } else {
+    logic = False;
+  }
+  return logic;
+}
+
+// add elements inside the list   =====================================================================================
+State add_begin(List* list, Student s) {
+  State state;
+  if (list == NULL || full_list(list) == True) {
+    state = Failed;
+  } else {
+    list->array[list->size] = list->array[0];
+    list->array[0] = s;
+    list->size+= 1;
+    state = Working;
+  }
+  return state;
+}
+
+State add_end(List* list, Student s) {
+  State state;
+  if (list == NULL || full_list(list) == True) {
+    state = Failed;
+  } else {
+    list->array[list->size] = s;
+    list->size+= 1;
+    state = Working;
+  }
+  return state;
+}
+
+State add_pos(List* list, int pos, Student s) {
+  State state;
+  if (list == NULL || full_list(list) == True || pos <= 0 || pos > list->max || pos > list->size+1) {
+    state = Failed;
+  } else {
+    list->array[list->size] = list->array[pos-1];
+    list->array[pos-1] = s;
+    list->size+= 1;
+    state = Working;
+  }
+  return state;
+}
+
+// remove elements from the list =================================================================================
+State remove_begin(List* list) {
+  State state;
+  if (list == NULL || empty_list(list) == True) {
+    state = Failed;
+  } else {
+    list->array[0] = list->array[list->size-1];
+    list->size-= 1;
+    state = Working;
+  }
+  return state;
+}
+
+State remove_end(List* list) {
+  State state;
+  if (list == NULL || empty_list(list) == True) {
+    state = Failed;
+  } else {
+    list->size-= 1;
+    state = Working;
+  }
+  return state;
+}
+
+State remove_pos(List* list, int pos) {
+  State state;
+  if (list == NULL || empty_list(list) == True || pos <= 0 || pos > list->max) {
+    state = Failed;
+  } else {
+    list->array[pos-1] = list->array[list->size-1];
+    list->size-= 1;
+    state = Working;
+  }
+  return state;
+}
+
+// Search for a element ===================================================================================
+State search_pos(List* list, int pos, Student *s) {
+  State state;
+  if (list == NULL || empty_list(list) == True || pos <= 0 || pos >= list->max) {
+    state = Failed;
+  } else {
+    *s = list->array[pos-1];
+    state = Working;
+  }
+  return state;
+}
+
+// Print the all list or the some struct Student
+State print_list(List* list) {
+  State state;
+  if (list == NULL) {
+    state = Failed;
+  } else {
+    if (empty_list(list) == True) {
+      printf("Empty List!\n\n");
+    } else {
+      for (int i = 0; i < list->size; i+= 1) {
+        print_student(list->array[i]);
+      }
+      printf("\n");
+    }
+    state = Working;
+  }
+  return state;
+}
+
+void print_student(Student s) {
+  printf("Reg: %d\n", s.reg);
+  printf("Name: %s\n", s.name);
+  printf("Grades: ");
+  for (int i = 0; i < 3; i+= 1) {
+    printf("%.1lf\t", s.grades[i]);
+  }
+  printf("\n");
+}
+
+
+
+
